f40cacd081d692ee3eb3adb00133445f1b891d5c
scolorado <224scolorado@frshd.com>
2023-10-02 09:29:17 -0400

    in the middle of vid #2

---
+++ sanbox.js
@@ -0,0 +1,34 @@
+//strings
+console.log('hello, world');
+
+let email = 'taylornation@gmail.com';
+console.log(email);
+
+//string concatentation
+let firstName = "Taylor";
+let lastname= "Swift";
+let fullName = firstName + ' ' + lastname;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+console.log(fullName[7]);
+
+//string length
+console.log(fullName.length);
+
+//string methods - the () mean it is a method
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+console.log(index);
+
+//common string methods
+
+let result1 = email.lastIndexOf('g');
+
+//end of tutorial 2
+
